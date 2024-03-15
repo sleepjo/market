6af60d6be454c5885805afcb1d04af024aae8528
kenryu <jryu1521@gmail.com>
2024-03-15 17:01:20 +0900

    1-1 done

--- src/main/java/sleepjo/xmlmapper/MemberService.java
+++ src/main/java/sleepjo/xmlmapper/MemberService.java
@@ -1,4 +1,39 @@
 package sleepjo.xmlmapper;
 
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.Map;
+
+import static sleepjo.common.Template.getSqlSession;
+
 public class MemberService {
+    private memberMapper mapper;
+
+    public void signUp(Map<String, String> data) {
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(memberMapper.class);
+        int result = mapper.signUp(data);
+        if(result >0){
+            System.out.println("sign up success...");
+            sqlSession.commit();
+        } else {
+            System.out.println("sign up failed...");
+            sqlSession.rollback();
+        }
+        sqlSession.close();
+    }
+
+    public void logIn(Object o) {
+    }
+
+    public void logOut() {
+    }
 }
+
+
+
+
+
+
+
+
---
+++ src/main/java/sleepjo/xmlmapper/memberMapper.java
@@ -0,0 +1,7 @@
+package sleepjo.xmlmapper;
+
+import java.util.Map;
+
+public interface memberMapper {
+    int signUp(Map<String, String> data);
+}
