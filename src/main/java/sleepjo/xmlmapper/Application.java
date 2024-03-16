843b1c58b4bf00d97bb558f61113a70d8595bb22
choseungyoun <tmddbs9313@naver.com>
2024-03-16 17:49:45 +0900

    [승윤 전체조회]

--- src/main/java/sleepjo/xmlmapper/Application.java
+++ src/main/java/sleepjo/xmlmapper/Application.java
@@ -1,5 +1,7 @@
 package sleepjo.xmlmapper;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import sleepjo.xmlmapper.MemberService;
 
@@ -11,7 +13,7 @@ public class Application {
     public static void main(String[] args) {
         sc = new Scanner(System.in);
         memberSerivce = new MemberService();
-        
+
         do{
             System.out.println("********Menu*********");
             System.out.println("1. Member submenu");
@@ -20,106 +22,108 @@ public class Application {
             System.out.println("9. Quit");
             System.out.print("input: ");
             input = sc.nextInt();
-            
+
             switch (input){
-                case 1: 
-                    memberSubmenu();
+                case 1:
+//                    memberSubmenu();
                     break;
-                case 2: 
+                case 2:
                     if(!logInStatus) {
                         System.out.println("You should log in first!!!!");
                         continue;
                     }
-                    
-                    productSubmenu();
+
+//                    productSubmenu();
                     break;
-                case 3: 
+                case 3:
                     postSubmenu();
                     break;
                 case 9: return;
             }
-            
-        } while(input != 9);
-        
-        
+
+
+     } while(input != 9);
+
+
     }
 
     private static void postSubmenu() {
-        do{
+        do {
             System.out.println("**********Post Submenu**********");
             System.out.println("1. view all posts");
-            System.out.println("2. view posts by title");
+            System.out.println("2. view posts by title");  //제목조회
             System.out.println("3. view posts by memberCode");
             System.out.println("9. back to main menu");
             input = sc.nextInt();
-            switch (input){
+            switch (input) {
                 case 1:
-                    viewAllPost();
+                    Post1Servise.viewAllPost();
                     break;
                 case 2:
-                    viewByTitle();
+//                    inputPostTitle();
                     break;
-                case 3:
-                    viewByMemberCode();
-                    break;
+//                case 3:
+//                    viewByMemberCode();
+//                    break;
                 case 9:
                     return;
             }
-        } while(input != 9); 
+        } while (input != 9);
     }
 
-    private static void viewByMemberCode() {
     }
-
-    private static void viewByTitle() {
-    }
-
-    private static void viewAllPost() {
-        
-    }
-
-    private static void productSubmenu() {
-        do{
-            System.out.println("**********Product Submenu**********");
-            System.out.println("1. insert product");
-            System.out.println("2. delete product");
-            System.out.println("3. update product");
-            System.out.println("9. back to main menu");
-            input = sc.nextInt();
-            switch (input){
-                case 1: 
-                    insertProduct();
-                    break;
-                case 2:
-                    deleteProduct();
-                    break;
-                case 3:
-                    updateProduct();
-                    break;
-                case 9:
-                    return;
-            }
-        } while(input != 9);
-        
-    }
-
-    private static void memberSubmenu() {
-        do{
-            System.out.println("**********Member Submenu**********");
-            System.out.println("1. Sign up");
-            System.out.println("2. Log in");
-            System.out.println("3. Log out");
-            System.out.println("9. Back to Main Menu");
-            System.out.print("input: ");
-            input = sc.nextInt();
-            
-            switch (input){
-                case 1: memberSerivce.signUp(); break;
-                case 2: memberSerivce.logIn(); break;
-                case 3: memberSerivce.logOut(); break;
-                case 9: return;
-            }
-            
-        } while(true);
-    }
-}
+//
+//    private static void viewByMemberCode() {
+//    }
+//
+//    private static void viewByTitle() {
+//    }
+//
+//    private static void viewAllPost() {
+//
+//    }
+//
+//    private static void productSubmenu() {
+//        do{
+//            System.out.println("**********Product Submenu**********");
+//            System.out.println("1. insert product");
+//            System.out.println("2. delete product");
+//            System.out.println("3. update product");
+//            System.out.println("9. back to main menu");
+//            input = sc.nextInt();
+//            switch (input){
+//                case 1:
+//                    insertProduct();
+//                    break;
+//                case 2:
+//                    deleteProduct();
+//                    break;
+//                case 3:
+//                    updateProduct();
+//                    break;
+//                case 9:
+//                    return;
+//            }
+//        } while(input != 9);
+//
+//    }
+//
+//    private static void memberSubmenu() {
+//        do{
+//            System.out.println("**********Member Submenu**********");
+//            System.out.println("1. Sign up");
+//            System.out.println("2. Log in");
+//            System.out.println("3. Log out");
+//            System.out.println("9. Back to Main Menu");
+//            System.out.print("input: ");
+//            input = sc.nextInt();
+//
+//            switch (input){
+//                case 1: memberSerivce.signUp(); break;
+//                case 2: memberSerivce.logIn(); break;
+//                case 3: memberSerivce.logOut(); break;
+//                case 9: return;
+//            }
+//
+//        } while(true);
+//    }
